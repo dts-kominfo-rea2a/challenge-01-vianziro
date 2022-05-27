75c492b0c5b9a3e661ea294a35dd67acb0a7c101
vianziro <vianziro@gmail.com>
2022-05-28 01:14:19 +0700

    update

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ const secondUser = {
 
             ],
             favoriteRestaurant: [
-               "Tempura Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"
+               "Tempura","Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"
             ]
 
 
